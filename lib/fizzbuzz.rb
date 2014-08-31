25b2044dd83af54374624bea6e9da2446666bef1
Nick Roberts <nickrhys@me.com>
2014-08-31 08:43:02 +0100

    seventh test passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,19 @@
-def divisable_by_3(number)
-	number % 3 == 0
+def divisable_by_3?(number)
+	divisable_by(number, 3)
 end
 
-def divisable_by_5(number)
-	number % 5 == 0 
+def divisable_by_5?(number)
+	divisable_by(number, 5)
 end
 
-def divisable_by_15(number)
-	number % 15 == 0
+def divisable_by_15?(number)
+	divisable_by(number, 15) 
 end
+
+def divisable_by(number,divisor)
+	number % divisor == 0
+end
+
+def fizzbuzz(number)
+	return "Fizz" if divisable_by_3?(number) 
+end	
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,26 +3,30 @@ require 'fizzbuzz'
 describe "Fizzbuzz" do
 
 	it "should know that 3 is divisable by 3" do
-		expect(divisable_by_3(3)).to eq true	
+		expect(divisable_by_3?(3)).to eq true	
 	end
 
 	it "should know that 4 is not divisable by 3" do 
-		expect(divisable_by_3(4)).to eq false
+		expect(divisable_by_3?(4)).to eq false
 	end
 
 	it "should know that 5 is divisable by 5" do
-		expect(divisable_by_5(5)).to eq true
+		expect(divisable_by_5?(5)).to eq true
 	end
 
 	it "should know that 6 is not divisable by 5" do
-		expect(divisable_by_5(6)).to eq false
+		expect(divisable_by_5?(6)).to eq false
 	end
 
 	it "should know that 15 is divisable by 3 and 5" do
-		expect(divisable_by_15(15)).to eq true
+		expect(divisable_by_15?(15)).to eq true
 	end
 
 	it "should know that 16 is not divisable by 3 and 5" do
-		expect(divisable_by_15(16)).to eq false
+		expect(divisable_by_15?(16)).to eq false
+	end
+
+	it "puts Fizz when divisable by 3" do
+		expect(fizzbuzz(3)).to eq "Fizz"		
 	end
 end
